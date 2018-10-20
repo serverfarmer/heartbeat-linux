a567ed64db5e31151efc1610de393f80e11c28b1
Tomasz Klim <github@tomaszklim.pl>
2018-10-20 22:02:49 +0200

    moved script from sf-standby-monitor extension

---
+++ scripts/checks/not-standby.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+devices=`/opt/heartbeat/scripts/facts/storage/list-physical-drives.sh |grep -v SSD |grep -vxFf /etc/heartbeat/skip-smart.sata`
+
+file=`mktemp -u /var/cache/heartbeat/usb.XXXXXXXXX.tmp`
+/opt/heartbeat/scripts/facts/storage/list-usb-drives.sh >$file
+
+for device in $devices; do
+	devname=`readlink -f $device`
+	if grep -qxF $devname $file && [ "`hdparm -C $device 2>&1 |grep standby`" = "" ]; then
+		echo $device
+	fi
+done
+
+rm -f $file
