af4e2a3e4b8ab8001e9443719d29c84d4702b8b8
Tomasz Klim <github@tomaszklim.pl>
2018-11-19 14:36:44 +0100

    fixed counting processes

--- scripts/checks/custom/count-processes.sh
+++ scripts/checks/custom/count-processes.sh
@@ -5,7 +5,7 @@ min=$2
 label=$3
 
 if [ "$label" != "" ]; then
-	count=`ps aux |grep "$search" |grep -v grep |wc -l`
+	count=`ps aux |grep "$search" |grep -v grep |grep -v count-processes.sh |wc -l`
 
 	if [[ $count -ge $min ]]; then
 		echo $label
