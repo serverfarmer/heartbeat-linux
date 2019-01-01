e4d456de02402419d014afc2535fd791cd21a558
Tomasz Klim <github@tomaszklim.pl>
2019-01-01 12:55:50 +0100

    disable smart check attempts for kingston datatraveler pen drives

--- scripts/facts/storage/list-udev-drives.sh
+++ scripts/facts/storage/list-udev-drives.sh
@@ -4,4 +4,4 @@
 # handled by udevd. It doesn't cover drives attached to any
 # hardware RAID controllers.
 
-ls /dev/disk/by-id/ata-* /dev/disk/by-id/usb-* 2>/dev/null |grep -v -- -part |grep -v VBOX |grep -v QEMU |grep -v VMware |grep -v CF_CARD |grep -v Cruzer |grep -v DVD
+ls /dev/disk/by-id/ata-* /dev/disk/by-id/usb-* 2>/dev/null |grep -v -- -part |grep -v VBOX |grep -v QEMU |grep -v VMware |grep -v CF_CARD |grep -v Cruzer |grep -v DataTraveler |grep -v DVD
